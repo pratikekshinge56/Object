11135e5f90ac1aadacd0ba295ffd02114e8c10d5
pratikekshinge56 <ekshingepratik1256@gmail.com>
2024-08-17 11:29:59 +0530

    Add files via upload

---
+++ Birthday.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string message = "Happy Birthday Jivlag🎂❤️‍🩹🍾🥳!";
+    for(int i = 1; i <= 1000; i++) {
+        std::cout << i << ". " << message << std::endl;
+    }
+
+    return 0;
+} 
